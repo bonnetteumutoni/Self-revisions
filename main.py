74eaaf61891a212c6a35fe80a555db93902a5412
Bonnette <bonnetteumu@gmail.com>
2025-05-27 11:59:18 +0300

    added new changes

--- main.py
+++ main.py
@@ -116,7 +116,7 @@ def disaster_planning(disaster):
     key="emergency"
     disasters=["fire","earthquake","floods"]
     agencies={"fire":"Fire department","earthquake":"Earthquake agency","floods":"Flood department","emergency":"Emergency department"}
-    supplies={"fire":"Exstinguishers","earthquake":"First-aid","floods":"Life Jackets","emergency":"Oxygen masks"}
+    supplies={"fire":"Extinguishers","earthquake":"First-aid","floods":"Life Jackets","emergency":"Oxygen masks"}
     if disaster in disasters:
         print(f"The disaster is {disaster} and agency is {agencies.get(disaster.lower())} and supplies are {supplies.get(disaster.lower())}")
     else:
@@ -125,4 +125,4 @@ def disaster_planning(disaster):
 disaster_planning("fire")
 disaster_planning("Eruption")
 
-# Find longest word in a list.
+
